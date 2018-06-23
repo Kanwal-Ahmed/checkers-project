400fee5bc160c5d77ecebec4afce4c664910e46d
Kanwal-Ahmed <37879936+Kanwal-Ahmed@users.noreply.github.com>
2018-06-23 13:29:34 +0500

    checkers project

---
+++ CheckersPlayer.h
@@ -0,0 +1,38 @@
+
+#include<iostream>
+#include<string.h>
+#include <fstream>
+#include<string>
+
+using namespace std;
+
+class CheckerPlayer
+{
+public:
+	CheckerPlayer();
+	CheckerPlayer(string w, string r);
+	CheckerPlayer(CheckerPlayer& copy);
+	void getplayername();
+	void showplayerdata();
+	int storeplayerdata();
+	void viewallrecord();
+	void turn();
+	void king_piece(char b[8][8]);// turns pieces into kings if conditions are met
+	
+	friend istream& operator >> (istream& is, CheckerPlayer& cp)
+	{
+		return is >> cp.white_player >> cp.red_player;
+	}
+
+	friend ostream& operator << (ostream& io, CheckerPlayer& cp)
+	{
+		return io << " " << cp.white_player << " " << cp.red_player;
+	}
+
+
+
+private:
+	string white_player;
+	string red_player;
+	int counter;
+};
--- main.cpp
+++ main.cpp
@@ -1,11 +1,46 @@
-#include<iostream>
-#include<string>
+#include "CheckersBoard.h"
+#include "CheckersPlayer.h"
+#include <iostream>
+#include <time.h>
+#include <ctype.h>
 using namespace std;
-#include "checkboard.h"
 
 int main()
 {
-	check k;
-	k.display();
+	cout << "\n\n----CHECKERS GAME DATA RECORD----\n" << endl;
+	CheckerPlayer p;
+	p.getplayername();
+	p.storeplayerdata();
+	cout << "\nHistory Of Players Tried Our Game:" << endl;
+	p.viewallrecord();
 	system("pause");
+	srand(time(0));
+	int i = 0, m = 0;
+	Board play_game;
+	play_game.instructions();
+	system("pause");
+	play_game.setpieces();
+	play_game.printb();
+	cout << "Computer Move...." << endl;
+	do
+	{
+		play_game.findp();
+		m = play_game.checkwin();
+		if (m == 0)
+		{
+			i = 100;
+		}
+		play_game.printb();
+		++i;
+		play_game.human_move();
+		m = play_game.checkwin();
+		if (m == 0)
+		{
+			i = 100;
+		}
+		play_game.printb();
+		++i;
+	} while (i < 100);
+
+ return 0;
 }
